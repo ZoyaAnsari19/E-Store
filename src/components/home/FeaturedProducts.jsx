7eb065814419f26e3251cb59a79907e8ace46711
Anees Shaikh <77051554+AveenoD@users.noreply.github.com>
2025-09-20 19:20:38 +0530

    Update FeaturedProducts.jsx

--- src/components/home/FeaturedProducts.jsx
+++ src/components/home/FeaturedProducts.jsx
@@ -10,9 +10,9 @@ const products = Array.isArray(data) ? data : [];
   return (
     <div className='bg-gray-100 p-3'>
       <Container>
-        <h1 className='text-center text-3xl font-bold'>
+{/*         <h1 className='text-center text-3xl font-bold'>
           Featured Products
-        </h1>
+        </h1> */}
         <div className="my-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3">
           {products.map((prod) => (
             <ProductBox key={prod.id} product={prod} />
